da535d987da2f26dfc334e6d7231e6de558b2b1f
Taha Ugurcan Ongel <tahagurcanoffical@gmail.com>
2023-03-19 20:18:51 +0300

    move related codes to their respective folders

--- src/FindTheTownJudge997/Solution.java
+++
@@ -1,4 +0,0 @@
-package FindTheTownJudge997;
-
-public class Solution {
-}
--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import CheckIfPangram1832.Solution;
-public class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        boolean result = solution.checkIfPangram("thequickbrownfoxjumpsoverthelazydog");
-        System.out.println(result);
-
-    }
-}
