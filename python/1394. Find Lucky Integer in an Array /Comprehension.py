700294ee11c9394d94120477bae3f53b408c7861
Taha Ugurcan Ongel <tahagurcanoffical@gmail.com>
2023-03-19 20:53:00 +0300

    solve 1394. Find Lucky Integer in an Array with list and dictionary comprehensions.

---
+++ python/1394. Find Lucky Integer in an Array /Comprehension.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findLucky(self, arr: list[int]) -> int:
+        # just to study comprehensions not a pretty solution by any means,
+        # but it is kinda cool that you can potentially do this with a one-liner
+        # return max([k if k == v else -1 for k, v in {i: arr.count(i) for i in arr}.items()])
+        dictionary = {i: arr.count(i) for i in arr}
+        solutions = [k if k == v else -1 for k, v in dictionary.items()]
+        return max(solutions)
