0c12148a40f32c12e5b8f64a64add0358ecdfc34
Taha Ugurcan Ongel <tahagurcanoffical@gmail.com>
2023-04-29 23:06:21 +0300

    Update solution of 94

---
+++ python/94. Binary Tree Inorder Traversal/Solution_Iterative.py
@@ -0,0 +1,20 @@
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> list[int]:
+        res = []
+        stack = []
+        cur = root
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        return res
---
+++ python/94. Binary Tree Inorder Traversal/Solution_Recursive.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> list[int]:
+        res = []
+
+        def inorder(root: TreeNode):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return res
